a2cf7f2e6f12e356290da0cfcdb841fd7f1a1e93
hibana2077 <hibana2077@gmaill.com>
2024-10-11 23:45:07 +0800

    Refactor main.py to add login functionality to admin panel

--- src/server/admin_panel/main.py
+++ src/server/admin_panel/main.py
@@ -3,3 +3,22 @@ import pandas as pd
 import requests
 
 st.title("HydroEdu Admin Panel")
+
+# Setting up session
+if "login" not in st.session_state:
+    st.session_state.login = False
+
+# Login
+if not st.session_state.login:
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+    if st.button("Login"):
+        response = requests.post("http://localhost:8000/login", json={"username": username, "password": password})
+        if response.status_code == 200:
+            st.session_state.login = True
+            st.success("Logged in successfully")
+            st.rerun()
+        else:
+            st.error("Invalid credentials")
+else:
+    st.write("Welcome to the admin panel")
